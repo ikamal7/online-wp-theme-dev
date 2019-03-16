1aa2b7765c96b9a491c66718e6b1b4ca7f29144f
Kamal <Kamal@DESKTOP-DIBAF6E>
2019-03-16 23:38:08 +0600

    ACF metabox

--- single.php
+++ single.php
@@ -7,16 +7,28 @@
     
             while(have_posts()){
                 the_post();
+
+                $has_website = get_field('has_website');
+                $book_name = get_field('book_name');
+
+                
+
                ?>
                <?php the_post_thumbnail('medium', 'd-block'); ?>
                 <h3><?php the_title(); ?></h3>
                 <h6><?php the_category(); ?></h6>
                 <p><?php the_tags(); ?></p>
                 <p><?php the_date(); ?></p>
-                <p>Writer: <?php echo get_post_meta(get_the_ID(), 'writer', true); ?></p>
-                <p>Published: <?php echo get_post_meta(get_the_ID(), 'Published', true); ?></p>
-                <p>facebook: <i class="fab fa-<?php echo get_post_meta(get_the_ID(), 'facebook', true); ?>-square"></i>  </p>
-                <p><?php the_content(); ?></p>
+                <?php if(!empty($book_name) ): ?>
+                <p><b>Book name:</b> <?php the_field('book_name'); ?></p>
+                <?php endif; ?>
+
+                <p><b>Book Summery: </b><?php the_field('book_summary'); ?></p>
+                <p><b>Book Published: </b><?php the_field('published_year'); ?></p>
+                <p><b>Book Rating: </b><?php the_field('rating'); ?></p>
+                <?php if($has_website == true) : ?>
+                <p><b>Book Website: </b><a href="<?php the_field('website_link'); ?>"><?php the_field('website_link'); ?></a></p>
+                <?php endif; ?>
                <?php
             }
             
